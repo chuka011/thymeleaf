62886fa95ee992f6e0cfce893750fcaba51dabe4
chuka011 <chuka011@gmail.com>
2024-08-17 10:15:58 +0200

    init project

---
+++ src/test/java/net/chuka011/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chuka011.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
