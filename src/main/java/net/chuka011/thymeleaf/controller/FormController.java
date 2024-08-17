cd0eb587671bbbb74d8377f6df4b2951d75a8c56
chuka011 <chuka011@gmail.com>
2024-08-17 20:38:26 +0200

    form--handle-form

--- src/main/java/net/chuka011/thymeleaf/controller/FormController.java
+++ src/main/java/net/chuka011/thymeleaf/controller/FormController.java
@@ -4,6 +4,8 @@ import net.chuka011.thymeleaf.model.UserForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -16,5 +18,12 @@ public class FormController {
         model.addAttribute("userForm", userForm);
         model.addAttribute("listProfession", List.of("Developer", "Tester", "Architect"));
         return "register-form";
+    }
+
+    @PostMapping("/register/save")
+    public String submitForm(Model model, @ModelAttribute("userForm") UserForm userForm) {
+
+        model.addAttribute("userForm", userForm);
+        return "register-sucess";
     }
 }
